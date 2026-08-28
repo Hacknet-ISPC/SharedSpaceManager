3139edf6f76b273deb74c4f52a1a74090b0d9585
Victor Oviedo <victor.oviedo.a87@gmail.com>
2026-08-28 16:57:58 -0300

    refactor: migrate dashboard-admin to Angular and split layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
+import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  {
+    path: 'admin',
+    component: AdminLayout,
+    children: [
+      { path: '', component: DashboardAdmin }
+    ]
+  }
 ];
