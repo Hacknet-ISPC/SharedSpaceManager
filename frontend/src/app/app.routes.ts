c13775cb59beb1f7c0b665988c07ea9cc166e295
RaringMoldxd <reinosomarcos92@gmail.com>
2026-08-28 13:28:16 -0300

    feat add navbar and footer componets

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home'; // Importas tu nuevo componente Home
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Esta línea le dice a Angular: "Cuando la URL sea '/', muestra la vista Home"
+  { path: '', component: Home }
+];
